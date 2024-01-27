3077f80512cb2f2c104005a627ecc6035b4c8977
Gardner <gardner@bickford.nz>
2024-01-28 02:40:44 +1300

    Add flexible DPO sharegpt strategy.

--- src/axolotl/prompt_strategies/dpo/chatml.py
+++ src/axolotl/prompt_strategies/dpo/chatml.py
@@ -1,6 +1,66 @@
+cat <<EOF > src/axolotl/prompt_strategies/dpo/chatml.py
 """
 DPO strategies for chatml
 """
+
+def fix_sample(sample):
+    possible_prompt_keys = ['instruction', 'question', 'input']
+    possible_chosen_keys = ['chosen_response', 'chosen']
+    possible_rejected_keys = ['rejected_response', 'rejected']
+
+    if not "prompt" in sample:
+        for key in possible_prompt_keys:
+            if key in sample:
+                sample["prompt"] = sample[key]
+                del sample[key]
+                break
+
+    if not "chosen" in sample:
+        for key in possible_chosen_keys:
+            if key in sample:
+                sample["chosen"] = sample[key]
+                del sample[key]
+                break
+
+    if not "rejected" in sample:
+        for key in possible_rejected_keys:
+            if key in sample:
+                sample["rejected"] = sample[key]
+                del sample[key]
+                break
+
+    return sample
+
+
+def flexible(
+    cfg,
+):  # pylint: disable=possibly-unused-variable,unused-argument
+    def transform_fn(sample):
+        sample = fix_sample(sample)
+        ## If the prompt isn't already in chatml format then convert it
+        if "<|im_start|>" not in sample["prompt"]:
+            if "system" in sample and sample["system"]:
+                sample["prompt"] = (
+                    f"<|im_start|>system\n{sample['system']}<|im_end|>\n"
+                    f"<|im_start|>user\n{sample['prompt']}<|im_end|>\n<|im_start|>assistant\n"
+                )
+            else:
+                sample[
+                    "prompt"
+                ] = f"<|im_start|>user\n{sample['prompt']}<|im_end|>\n<|im_start|>assistant\n"
+
+        ## Sometimes the chosen and rejected responses already have the <|im_end|> string
+        ## If not then add it:
+        if "<|im_end|>" not in sample["chosen"]:
+            sample["chosen"] = f"{sample['chosen']}<|im_end|>"
+
+        if "<|im_end|>" not in sample["rejected"]:
+            sample["rejected"] = f"{sample['rejected']}<|im_end|>"
+
+        return sample
+
+    return transform_fn
+
 
 
 def argilla(
@@ -108,3 +168,5 @@ def ultra(cfg):  # pylint: disable=possibly-unused-variable,unused-argument
         return sample
 
     return transform_fn
+
+EOF
